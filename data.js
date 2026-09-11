7a8495a8727d7a0585d06f3793d7867b6931072f
tryitindia <gpsplacementcounsultancy@gmail.com>
2026-09-11 12:16:37 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -3,66 +3,66 @@ const PRODUCTS = [
     "id": 1,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 01",
-    "image": "product-01.jpeg"
+    "image": "images/product-01.jpeg"
   },
   {
     "id": 2,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 02",
-    "image": "product-02.jpeg"
+    "image": "images/product-02.jpeg"
   },
   {
     "id": 3,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 03",
-    "image": "product-03.jpeg"
+    "image": "images/product-03.jpeg"
   },
   {
     "id": 4,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 04",
-    "image": "product-04.jpeg"
+    "image": "images/product-04.jpeg"
   },
   {
     "id": 5,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 05",
-    "image": "product-05.jpeg"
+    "image": "images/product-05.jpeg"
   },
   {
     "id": 6,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 06",
-    "image": "product-06.jpeg"
+    "image": "images/product-06.jpeg"
   },
   {
     "id": 7,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 07",
-    "image": "product-07.jpeg"
+    "image": "images/product-07.jpeg"
   },
   {
     "id": 8,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 08",
-    "image": "product-08.jpeg"
+    "image": "images/product-08.jpeg"
   },
   {
     "id": 9,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 09",
-    "image": "product-09.jpeg"
+    "image": "images/product-09.jpeg"
   },
   {
     "id": 10,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 10",
-    "image": "product-10.jpeg"
+    "image": "images/product-10.jpeg"
   },
   {
     "id": 11,
     "category": "Our Products",
     "name": "TRY IT INDIA Product 11",
-    "image": "product-11.jpeg"
+    "image": "images/product-11.jpeg"
   }
 ];
